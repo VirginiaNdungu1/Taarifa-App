8f5efcc6a3af19564c54846eb6a49b9520cfbd95
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-21 11:59:27 +0300

    Create app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,15 @@
 # initialise flask
 from flask import Flask
+# initialise configurations
+from config import config_options
+
+# setting up configurations
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Create app configurations
+    app.config.from_object(config_options[config_name])
+
+    return app
