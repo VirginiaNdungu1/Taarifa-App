7f78e0009ceec0324fed25ef883cc115a5a2d68d
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-21 12:27:50 +0300

    Create newsSource Model to create News Source Objects

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class newsSource:
+    '''
+    News Source Class to define News Source Objects
+    '''
+
+    def __init__(self, id, name, description, logo):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.logo = logo
