b3d6e21bc1c564fadd80c21d846cb88d6e4b0f1c
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-21 11:55:58 +0300

    Creating configuration Options Setting environment variables

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+
+class Config:
+    SOURCES_API_BASE_URL = 'https://newsapi.org/v1/sources?language={}&category={}&country={}&apiKey={}'
+
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v1/articles?source={}&sortBy={}&apiKey={}'
+
+    # set environment variables
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+# Create a dictionary to help us access different configuration option classes
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
+
+'
