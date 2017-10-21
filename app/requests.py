7db0ef0c700385538fb6e0859a71e973870ff0c3
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-21 12:51:43 +0300

    Create function that gets all the news sources

--- app/requests.py
+++ app/requests.py
@@ -25,3 +25,20 @@ def configure_request(app):
     '''
     get the News Sources Url from the config Object
     '''
+
+
+def get_sources(language, category, country):
+    '''
+    Function that gets the json response to the ur request
+    '''
+    get_sources_url = base_url.format(language, category, country, apiKey)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+        if get_sources_response['results']:
+            sources_results_list = get_sources_response['results']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
