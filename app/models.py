b08ce34207e469dea38e7346625caff72d77e891
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-22 08:54:48 +0300

    Update models - create Articles model class

--- app/models.py
+++ app/models.py
@@ -9,3 +9,16 @@ class news_source:
         self.homepage_url = homepage_url
         self.description = description
         # self.logo = logo
+
+
+class Articles:
+    '''
+    Articles class to define Source's Articles Objects
+    '''
+
+    def __init__(self, author, title, description, logo, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = title
+        self.logo = 'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/10/' + logo
+        self.publishedAt = publishedAt
