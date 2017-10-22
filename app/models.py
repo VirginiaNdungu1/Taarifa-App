ac1763368a7769a58076c96885cb77ac79d13cb4
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-10-23 00:16:17 +0300

    Update models - Articles

--- app/models.py
+++ app/models.py
@@ -16,10 +16,11 @@ class Articles:
     Articles class to define Source's Articles Objects
     '''
 
-    def __init__(self, source_id, author, title, description, logo, publishedAt):
-        self.source_id = source_id
+    def __init__(self, author, title, description, article_url, logo, publishedAt):
+        # self.source_id = source
         self.author = author
         self.title = title
-        self.description = title
-        self.logo = 'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/10/' + logo
+        self.description = description
+        self.article_url = article_url
+        self.logo = logo
         self.publishedAt = publishedAt
